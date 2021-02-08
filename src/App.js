25953e60c8a53081d3b051b6b20c744fedba4a3d
Laszlo Lazuer <visualjazz@gmail.com>
2021-02-08 17:28:31 -0600

    chore: stub react

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+function App() {
+
+  const progress = 20;
+  const value="20";
+
+  return (
+    <div className="App">
+      <h1>IA 2021!</h1>
+    </div>
+  );
+}
+
+export default App;
